53e73dc93ac13c8185d7d13afd14ca42d90c50e5
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 20:27:28 -0600

    code complete.

---
+++ ConsoleUI/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI;
+
+public static class Methods
+{
+    public static void RunsALot()
+    {
+        long total = 0;
+        int test = 0;
+
+        for (int i = -1000; i < 1000; i++)
+        {
+            total += i;
+            try
+            {
+                test = 5 / i;
+            }
+            catch
+            {
+                Console.WriteLine("There was an exception.");
+            }
+        }
+
+        Console.WriteLine($"The total is {total}");
+    }
+}
